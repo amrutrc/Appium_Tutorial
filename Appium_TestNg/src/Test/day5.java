f8bf75feb9dd18d8a5501511a5640c1f9df15eae
amrut <amrutc010@gmail.com>
2020-08-16 18:45:01 +0530

    Commited new code

---
+++ Appium_TestNg/src/Test/day5.java
@@ -0,0 +1,20 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class day5 
+{
+	@Test
+	public void Demo1()
+	{
+		System.out.println("Demo1");
+	}
+
+	@Test
+	public void Demo2()
+	{
+		System.out.println("Demo2");
+	}
+	
+	
+}
